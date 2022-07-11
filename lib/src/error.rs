268e99398fd6ef8f8962fa0f883752f17a1bdb85
Lukas Krickl <lukas@krickl.dev>
2022-07-11 14:58:27 +0200

    Added test for crc

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Eq, PartialEq)]
 pub enum Error {
     NusCrcNotEnoughData,
 }
--- lib/src/nusheader.rs
+++ lib/src/nusheader.rs
@@ -1,5 +1,3 @@
-use std::ascii::AsciiExt;
-
 use crate::error::Error;
 
 pub struct NusHeader {
@@ -7,7 +5,7 @@ pub struct NusHeader {
     clck_rate: u32,
     boot_addr: u32,
     lu_ver: u32,
-    crc: u64,
+    crc: (u32, u32),
     reserved_1: u64,
     // will be cut off after 0x14 bytes,
     // therefore spilling into the
@@ -43,7 +41,6 @@ const HEADER_SIZE: usize = 0x40;
 const CRC_START: usize = 0x1000;
 const CRC_LEN: usize = 0x100000;
 const TITLE_LEN: usize = 0x14;
-const CRC_START_VAL: u32 = 0xf8ca4ddc;
 
 impl NusHeader {
     /// Converts the NusHeader to
@@ -55,7 +52,8 @@ impl NusHeader {
         header.extend(self.clck_rate.to_be_bytes());
         header.extend(self.boot_addr.to_be_bytes());
         header.extend(self.lu_ver.to_be_bytes());
-        header.extend(self.crc.to_be_bytes());
+        header.extend(self.crc.0.to_be_bytes());
+        header.extend(self.crc.1.to_be_bytes());
         header.extend(self.reserved_1.to_be_bytes());
 
         // Title always needs to be exactly 0x14 bytes
@@ -77,96 +75,118 @@ impl NusHeader {
         })
     }
 
+    pub fn set_crc(&mut self, data: &[u8]) -> Result<(u32, u32), Error> {
+        let crc = Self::crc(data)?;
+        self.crc = crc;
+        Ok(crc)
+    }
+
     /// Calculates the nus crc sum
     /// similar to libdragon's implementation of chksm64
-    pub fn crc(&mut self, data: &[u8]) -> Result<u64, Error> {
+    pub fn crc(data: &[u8]) -> Result<(u32, u32), Error> {
         if data.len() < CRC_LEN + CRC_START {
             return Err(Error::NusCrcNotEnoughData);
         }
 
-        let base = 0xffffffff;
+        let mut t8;
+        let mut t6;
+        let mut t7;
+        let mut t9;
 
-        let (mut t0, mut t1, mut t2, mut t3, mut t4, mut t5, mut t6, mut t7, mut t8, mut t9) = (
-            0_u32, 0_u32, 0_u32, 0_u32, 0_u32, 0_u32, 0_u32, 0_u32, 0_u32, 0_u32,
-        );
-        let (mut s0, mut s6) = (0_u32, 0_u32);
-        let (mut a0, mut a1, mut a2, mut a3, mut at) = (0_u32, 0_u32, 0_u32, 0_u32, 0_u32);
-        let mut lo = 0;
-        let (mut v0, mut v1) = (0, 0);
-        let mut ra = 0;
+        let mut v1;
 
-        let mut s6 = 0x3f;
+        let s6 = 0x3f;
         let mut a0 = 0x1000;
 
-        a1 = s6;
-        at = 0x5d588b65;
+        let mut a1: u32 = s6;
+        let mut at = 0x5d588b65;
 
-        lo = (a1.wrapping_mul(at)) & base;
-        ra = 0x100000;
-        v1 = 0;
-        t0 = 0;
-        t1 = a0;
-        t5 = 32;
-        v0 = lo;
-        v0 = (v0 + 1) & base;
-        a3 = v0;
-        t2 = v0;
-        t3 = v0;
-        s0 = v0;
-        a2 = v0;
-        t4 = v0;
+        let lo = a1.wrapping_mul(at);
+        let ra = 0x100000;
+        let mut t0 = 0;
+        let mut t1 = a0;
+        let t5 = 32_u32;
+        let mut v0 = lo;
+        v0 = v0 + 1;
+        let mut a3 = v0;
+        let mut t2 = v0;
+        let mut t3 = v0;
+        let mut s0 = v0;
+        let mut a2 = v0;
+        let mut t4 = v0;
 
         while t0 != ra {
             // TODO dont unwrap
             v0 = u32::from_be_bytes(data[t1 as usize..t1 as usize + 4].try_into().unwrap());
-            v1 = (a3 + v0) & base;
+            v1 = a3.wrapping_add(v0);
             at = (v1 < a3).into();
             a1 = v1;
 
             if at != 0 {
-                t2 = (t2 + 1) & base;
+                t2 = t2.wrapping_add(1);
             }
 
             v1 = v0 & 0x1F;
-            t7 = (t5 - v1) & base;
+            t7 = t5.wrapping_sub(v1);
             t8 = v0 >> t7;
-            t6 = (v0 << v1) & base;
+            t6 = v0 << v1;
             a0 = t6 | t8;
             at = (a2 < v0).into();
             a3 = a1;
-            t3 = (t3 ^ v0) & base;
-            s0 = (s0 + a0) & base;
+            t3 ^= v0;
+            s0 = s0.wrapping_add(a0);
             if at != 0 {
-                t9 = (a3 ^ v0) & base;
+                t9 = a3 ^ v0;
 
-                a2 = (a2 ^ t9) & base;
+                a2 ^= t9;
             } else {
-                a2 = (a2 ^ a0) & base;
+                a2 ^= a0;
             }
 
             t0 += 4;
-            t7 = (v0 ^ s0) & base;
+            t7 = v0 ^ s0;
             t1 += 4;
-            t4 = (t4 + t7) & base;
+            t4 = t4.wrapping_add(t7);
         }
 
-        t6 = (a3 ^ t2) & base;
-        a3 = (t6 ^ t3) & base;
-        t8 = (s0 ^ a2) & base;
-        s0 = (t8 ^ t4) & base;
+        t6 = a3 ^ t2;
+        a3 = t6 ^ t3;
+        t8 = s0 ^ a2;
+        s0 = t8 ^ t4;
 
-        Ok((a3 as u64) << 32 | s0 as u64)
+        Ok((a3, s0))
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::NusHeader;
+    use super::*;
+
+    const CRC_END: usize = CRC_START + CRC_LEN;
 
     #[test]
-    fn convert_to_bytes() {
-        let h = NusHeader::default();
+    fn crc_fail() {
+        assert_eq!(Err(Error::NusCrcNotEnoughData), NusHeader::crc(&[0, 1, 2]));
 
-        h.to_bytes();
+        // test failure off by 1
+        let mut test_data = vec![];
+        for i in 0..CRC_END - 1 {
+            test_data.push(i as u8);
+        }
+        assert_eq!(Err(Error::NusCrcNotEnoughData), NusHeader::crc(&test_data));
+    }
+
+    #[test]
+    fn crc() {
+        let mut test_data = vec![];
+
+        // 0, 1, 2, 3, ... 255, 0, 1, ...
+        for i in 0..CRC_END {
+            test_data.push(i as u8);
+        }
+
+        // result dervice from calculating the checksum
+        // using the legacy python script
+        assert_eq!(Ok((4207429594, 3000934689)), NusHeader::crc(&test_data));
     }
 }
