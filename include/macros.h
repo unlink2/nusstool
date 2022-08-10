a10bd492722a8f30104335167dea8d1009a22708
Lukas Krickl <lukas@krickl.dev>
2022-08-10 11:25:40 +0200

    Added memory debugger template

--- include/macros.h
+++ include/macros.h
@@ -11,4 +11,23 @@
 #include <cmocka.h>
 #endif
 
+#ifdef DEBUG_MALLOC
+
+/**
+ * This is an example of how to debug malloc
+ * Simply macro alloc, record allocs and free in some way and
+ * maybe over-allocate and put a magic number at the end of the allocation
+ * to detect out of bounds writes!
+ */
+
+#include "types.h"
+
+#define malloc(n) debug_malloc(n, __FILE__, __LINE__)
+#define free(n) debug_free(n, __FILE__, __LINE__)
+
+void *debug_malloc(usize size, char *file, usize line);
+void debug_free(void *ptr, char *file, usize line);
+
+#endif
+
 #endif
